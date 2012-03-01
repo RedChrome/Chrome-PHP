c671dd5f4ae12100d0ed31674b6fe26798f9bcd1
Alexander Book <redchrome@gmx.de>
2012-03-01 17:59:32 +0100

    Changed License and added database scheme Signed-off-by: Alexander Book <redchrome@gmx.de>

--- include/lib/core/design/factory/decorator/composite/body.php
+++ include/lib/core/design/factory/decorator/composite/body.php
@@ -5,16 +5,16 @@
  *
  * LICENSE
  *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
+ * This source file is subject to the Creative Commons license that is bundled
+ * with this package in the file LICENSE.
  * It is also available through the world-wide-web at this URL:
- * http://chrome-php.de/license/new-bsd
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
  * If you did not receive a copy of the license AND are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@chrome-php.de so we can send you a copy immediately.
  *
  * @package    CHROME-PHP
- * @copyright  Copyright (c) 2008-2009 Chrome - PHP (http://www.chrome-php.de)
+ * @copyright  Copyright (c) 2008-2012 Chrome - PHP (http://www.chrome-php.de)
  * @license    http://chrome-php.de/license/new-bsd        New BSD License
  * @version    $Id: 0.1 beta <!-- phpDesigner :: Timestamp [30.03.2010 20:33:27] --> $
  */
--- include/modules/content/user/login/include.php
+++ include/modules/content/user/login/include.php
@@ -5,18 +5,18 @@
  *
  * LICENSE
  *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
+ * This source file is subject to the Creative Commons license that is bundled
+ * with this package in the file LICENSE.
  * It is also available through the world-wide-web at this URL:
- * http://chrome-php.de/license/new-bsd
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
  * If you did not receive a copy of the license AND are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@chrome-php.de so we can send you a copy immediately.
  *
  * @package    CHROME-PHP
  * @subpackage Chrome.User
- * @copyright  Copyright (c) 2008-2009 Chrome - PHP (http://www.chrome-php.de)
- * @license    http://chrome-php.de/license/new-bsd		New BSD License
+ * @copyright  Copyright (c) 2008-2012 Chrome - PHP (http://www.chrome-php.de)
+ * @license    http://creativecommons.org/licenses/by-nc-sa/3.0/ Create Commons
  * @version    $Id: 0.1 beta <!-- phpDesigner :: Timestamp [01.03.2012 17:03:31] --> $
  * @author     Alexander Book
  */
